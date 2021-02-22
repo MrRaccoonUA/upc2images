43876fbe0f52838b2ec36918788cf857b293f683
Platon Vovchenko <44032917+MrRaccoonUA@users.noreply.github.com>
2021-02-22 22:54:09 +0200

    Made function link_extractor (#3)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import pandas as pd
 import requests
 import json
 import time
 import logging
-from datetime import datetime
+import re
+from collections import namedtuple
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, filename='log_report.txt',
@@ -11,48 +11,89 @@ logging.basicConfig(level=logging.DEBUG, filename='log_report.txt',
                     datefmt='%Y-%m-%d %I:%M:%S')
 
 
-def load_data(file_path):
-    return pd.read_csv(file_path, sep=",", header=None)
-
-
-def api_search(list_of_upc):
-    """
-
-    :param list_of_upc: list of the UPCs that we need to lookup
-    :return:
-    """
-    API_COOL_DOWN_TIMEOUT = 11
-    df_upc_images = pd.DataFrame(data={'UPC': [], 'Product Name': [], 'Images links': []})
+def api_search(code, API_URL):
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'user_key': 'only_for_dev_or_pro',
         'key_type': '3scale'
     }
-    for code in list_of_upc:
-        start_time = datetime.now()
-        resp = requests.get('https://api.upcitemdb.com/prod/trial/lookup?upc=' + code, headers=headers)
-        data = json.loads(resp.text)
-        time.sleep(API_COOL_DOWN_TIMEOUT)
-        if data['code'] == 'OK':
-            for item in data['items']:
-                df_upc_images = df_upc_images.append({'UPC': item['ean'], 'Product Name': item['title'],
-                                                     'Images links': item['images']}, ignore_index=True)
-        logger.info(f'{list_of_upc.index(code)} UPC: {code} - Status: {data["code"]}. '
-                    f'Iteration time: {datetime.now() - start_time}')
-        df_upc_images['UPC'] = df_upc_images['UPC'].apply(int)
-    return df_upc_images
+    resp = requests.get(API_URL + code, headers=headers)
+    data = json.loads(resp.text)
+    logger.info(f'UPC: {code} - Status: {data["code"]}')
+    if data['code'] != 'OK':
+        return None
+    product_info = namedtuple('Product', 'UPC Product_Name Images_Links')
+    product_info = product_info(data['items'][0]['ean'], data['items'][0]['title'], data['items'][0]['images'])
+    return product_info
+
+
+def link_extractor(upc, list_of_links):
+    def extractor(link, prefix):
+        word = len(prefix)
+        for item in link:
+            if item[:word] == prefix:
+                yield item
+
+    """
+    prefix: Start of the link which we need to find in the list of the links and extract all link with this start.
+    https://target. - start of the Target link
+    https://i5. - start of the Walmart link
+    """
+
+    def target_extractor(TARGET_DEFAULT_SIZE, TARGET_MAX_SIZE, target_prefix):
+        link = namedtuple('Link', 'resized_link')
+        resized_link = re.sub(str(TARGET_DEFAULT_SIZE), str(TARGET_MAX_SIZE), target_prefix[0])
+        link = link(resized_link)
+        logger.info(f'UPC: {upc} - Status: Image OK')
+        return link
+
+    def walmart_extractor(walmart_prefix):
+        link = namedtuple('Link', 'resized_link')
+        resized_link = re.sub(r"[?].+", "", walmart_prefix[0])
+        link = link(resized_link)
+        logger.info(f'UPC: {upc} - Status: Image OK')
+        return link
+
+    TARGET_DEFAULT_SIZE = 1000
+    TARGET_MAX_SIZE = 3000
+    prefix = 'https://target.'
+    target_prefix = list(extractor(list_of_links, prefix))
+    if target_prefix:
+        return target_extractor(TARGET_DEFAULT_SIZE, TARGET_MAX_SIZE, target_prefix)
+
+    prefix = 'https://i5.'
+    walmart_prefix = list(extractor(list_of_links, prefix))
+    if walmart_prefix:
+        return walmart_extractor(walmart_prefix)
+
+    return logger.info(f'UPC: {upc} - Status: No image')
 
 
 if __name__ == '__main__':
     upc_list = []
     n = int(input("Enter number of elements: "))
     print("Enter the elements by row")
+
     for i in range(0, n):
         ele = int(input())
         upc_list.append(ele)
-
     upc_list = [str(x) for x in upc_list]
-    print('Start searching...')
-    df_images = api_search(upc_list)
-    df_images.to_json('Report.json', orient="records")
+
+    API_URL_GET = 'https://api.upcitemdb.com/prod/trial/lookup?upc='
+
+    for code in upc_list:
+        start_time = time.time()
+
+        detail = api_search(code, API_URL_GET)
+        if not detail:
+            continue
+        print(type(detail.Images_Links))
+        extracted_link = link_extractor(detail.UPC, detail.Images_Links)
+        print(extracted_link)
+
+        API_COOL_DOWN_TIMEOUT = 10
+        iteration_time = time.time() - start_time
+        cool_down = API_COOL_DOWN_TIMEOUT - iteration_time
+        if len(upc_list) > 1 and code != upc_list[-1]:
+            time.sleep(cool_down)
--- test-api-search.py
+++ test-api-search.py
@@ -1,20 +1,16 @@
 import unittest
 from main import api_search
-import pandas as pd
-from pandas._testing import assert_frame_equal
+from collections import namedtuple
 
 
 class TestStringMethods(unittest.TestCase):
 
     def test_api_search(self):
-        df = pd.read_csv('test-data/test-data.txt', sep=",", header=None)
-        upc_list = []
-        for el in df[0]:
-            upc_list.append(el)
-        upc_list = [str(x) for x in upc_list]
-        expected_df_report = pd.read_json('Report.json')
-        actual_df_report = api_search(upc_list)
-        assert_frame_equal(actual_df_report, expected_df_report)
+        code = '884394007308'
+        Product = namedtuple('Product', 'UPC Product_Name Images_Links')
+        expected_result = Product(UPC='0884394007308', Product_Name='OKF Farmers Aloe Drink, Original, 50.7 Fl Oz', Images_Links=['https://i5.walmartimages.com/asr/20206185-284f-4e84-bba8-6243f97b65f3_1.0eeeaf4c0e89999e4ad28b38fcef5552.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff', 'https://jetimages.jetcdn.net/md5/5031713635ec2e16a5229e8d7e6bbcce.500'])
+        actual_result = api_search(code, API_URL='https://api.upcitemdb.com/prod/trial/lookup?upc=')
+        self.assertEqual(actual_result, expected_result)
 
 
 if __name__ == '__main__':
